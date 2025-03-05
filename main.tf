40df48c1f0229f94b3971de842f15f3fbd7fe61a
jahnavi <jahnavi.pithadiya.411@gmail.com>
2025-03-05 21:33:43 +0530

    solving

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"  # Use the latest stable version you need
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  alias = "us-easst-1"
+}
+
 module "vpc" {
   source = "./modules/vpc"
   vpc_cidr = var.vpc_cidr
