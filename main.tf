6e3af15a939b6dd974c3a7f7cceab4d759c5653c
jahnavi <jahnavi.pithadiya.411@gmail.com>
2025-03-05 21:36:30 +0530

    solving

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"  # Use the latest stable version you need
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
   alias = "us-easst-1"
