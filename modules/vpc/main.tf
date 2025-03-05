6039fa2a6e417d01fb90a0d1a02628b9f66ea689
jahnavi <jahnavi.pithadiya.411@gmail.com>
2025-03-05 22:23:28 +0530

    addd

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_subnet" {
 
 
 resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.terra_vpc.id
   cidr_block              = var.public_subnet_2_cidr
   availability_zone       = var.az2
   map_public_ip_on_launch = true
