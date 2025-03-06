73c33f9fca398049d5c794b5294cd39cf6408017
jahnavi <jahnavi.pithadiya.411@gmail.com>
2025-03-06 10:33:42 +0530

    addd

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -100,7 +100,7 @@ resource "aws_security_group" "alb_sg" {
         from_port   = 5432
         to_port     = 5432
         protocol    = "tcp"
-        security_groups = [aws_security_group.ec2_sg.id]  # Only allow EC2 instances to connect
+        security_groups = [var.allow_all]  # Only allow EC2 instances to connect
     }
     ingress {
         from_port   = 8000
