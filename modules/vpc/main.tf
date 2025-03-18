fe14cd15287e102f2abe5ec6b42ba39bbeb5ed9d
jahnavi <jahnavi.pithadiya.411@gmail.com>
2025-03-18 14:14:02 +0530

    ad

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -132,7 +132,7 @@ resource "aws_security_group" "ec2_sg" {
         from_port   = 8000
         to_port     = 8000
         protocol    = "tcp"
-        security_groups = "0.0.0.0/0"
+        cidr_blocks      = [var.allow_all]
     }
 
     egress {
